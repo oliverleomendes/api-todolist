9e51af6e96e7b10d8f5bdc1b14ed8068a3edb9ae
LEONARDO MENDES DE OLIVEIRA <leonardo.moliveira25@senacsp.edu.br>
2025-02-05 19:33:32 -0300

    Criação do reposiório e primeira sincronização

---
+++ src/controllers/TwoFaController.js
@@ -0,0 +1,27 @@
+const speakeasy = require('speakeasy')
+const qrcode = require('qrcode')
+
+class TwoFaController{
+
+    gerarToken(request, response){
+        const secret = speakeasy.generateSecret();
+        qrcode.toDataURL(secret.otpauth_url, (err, data_url) => {
+            response.json({token: secret.base32, qr_code: data_url})
+        })
+    }   
+    
+    validarToken(request, response){
+        const { token, secret } = request.body
+
+        const verified = speakeasy.totp.verify({
+            secret: secret,
+            encoding: 'base32',
+            token: token
+        });
+
+        response.json({ verified })
+    }
+
+}
+
+module.exports = new TwoFaController()
---
+++ src/routes/routes.js
@@ -0,0 +1,25 @@
+const connection = require('../database/connection')
+const express = require('express')
+const router = express.Router()
+const TaskController = require('../controllers/TaskController')
+const UserController = require('../controllers/UserController')
+const verificarToken = require('../middleware/authMiddleware')
+const TwoFaController = require('../controllers/TwoFaController')
+
+router.post('/usuario/criar', UserController.cadastrarUsuario)
+router.post('/usuario/autenticar', UserController.autenticarUsuario)
+router.get('/usuarios', verificarToken, UserController.listarUsuarios)
+router.get('/usuario/:id', verificarToken, UserController.listarUmUsuario)
+router.put('/usuario/atualizar/:id', verificarToken, UserController.atualizarUsuario)
+router.delete('/usuario/excluir/:id', verificarToken, UserController.removerUsuario)
+
+router.get('/2fa/gerar', TwoFaController.gerarToken)
+router.post('/2fa/validar', TwoFaController.validarToken)
+
+router.post('/tarefa/criar', verificarToken, TaskController.novaTarefa)
+router.get('/tarefas', verificarToken, TaskController.listarTarefas)
+router.get('/tarefa/:id', verificarToken, TaskController.listarUmaTarefa) 
+router.put('/tarefa/atualizar/:id', verificarToken, TaskController.atualizarTarefa)
+router.delete('/tarefa/excluir/:id', verificarToken, TaskController.removerTarefa)
+
+module.exports = router
